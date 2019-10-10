bd43f8d95887494edf27349b09d81c1a0aaf66ca
Maciej <betterlifethanksapp@gmail.com>
2019-10-10 14:10:00 +0200

    Create CurrentFragment and ViewModel

---
+++ app/src/main/java/betterlifethanksapp/gmail/com/locationWakeUp/ui/CurrentRouteFragment.kt
@@ -0,0 +1,33 @@
+package betterlifethanksapp.gmail.com.locationWakeUp.ui
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import betterlifethanksapp.gmail.com.locationWakeUp.R
+
+class CurrentRouteFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = CurrentRouteFragment()
+    }
+
+    private lateinit var viewModel: CurrentRouteViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.current_route_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(CurrentRouteViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/betterlifethanksapp/gmail/com/locationWakeUp/ui/CurrentRouteViewModel.kt
@@ -0,0 +1,7 @@
+package betterlifethanksapp.gmail.com.locationWakeUp.ui
+
+import androidx.lifecycle.ViewModel
+
+class CurrentRouteViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
