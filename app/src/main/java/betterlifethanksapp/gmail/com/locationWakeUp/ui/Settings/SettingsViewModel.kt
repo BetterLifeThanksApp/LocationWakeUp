e6a97924bda890974b8c7fb842c169f4933d1ce9
Maciej <betterlifethanksapp@gmail.com>
2019-10-10 14:14:43 +0200

    Create SettingsFragment and ViewModel

---
+++ app/src/main/java/betterlifethanksapp/gmail/com/locationWakeUp/ui/Settings/SettingsViewModel.kt
@@ -0,0 +1,7 @@
+package betterlifethanksapp.gmail.com.locationWakeUp.ui.Settings
+
+import androidx.lifecycle.ViewModel
+
+class SettingsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
