337ab6ed745623139de55477170df6cb8d95a742
Maciej <betterlifethanksapp@gmail.com>
2019-10-11 12:38:12 +0200

    Initial Apperance and switch between other Fragments works :)

--- app/src/main/java/betterlifethanksapp/gmail/com/locationWakeUp/ui/MainActivity.kt
+++ app/src/main/java/betterlifethanksapp/gmail/com/locationWakeUp/ui/MainActivity.kt
@@ -2,12 +2,39 @@ package betterlifethanksapp.gmail.com.locationWakeUp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import betterlifethanksapp.gmail.com.locationWakeUp.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController:NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        navController = Navigation.findNavController(
+            this,
+            R.id.nav_host_fragment)
+
+        bottom_nav.setupWithNavController(navController)
+
+        NavigationUI.setupActionBarWithNavController(
+            this,
+            navController
+        )
+
+
     }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController,null)
+    }
+
 }
