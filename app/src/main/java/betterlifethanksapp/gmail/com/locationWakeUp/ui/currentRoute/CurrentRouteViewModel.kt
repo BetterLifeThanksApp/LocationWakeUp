e42ac3a5e477577af3808598b4630662ce38607f
Maciej <betterlifethanksapp@gmail.com>
2020-01-07 14:39:14 +0100

    Delete old  data(databinding) from CurrentRouteViewModel

--- app/src/main/java/betterlifethanksapp/gmail/com/locationWakeUp/ui/currentRoute/CurrentRouteViewModel.kt
+++ app/src/main/java/betterlifethanksapp/gmail/com/locationWakeUp/ui/currentRoute/CurrentRouteViewModel.kt
@@ -1,35 +1,12 @@
 package betterlifethanksapp.gmail.com.locationWakeUp.ui.currentRoute
 
-import androidx.databinding.Bindable
-import androidx.databinding.Observable
-import androidx.databinding.PropertyChangeRegistry
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+
+
 import androidx.lifecycle.ViewModel
 
-class CurrentRouteViewModel() : ViewModel(), Observable {
-
-    private val callbacks:PropertyChangeRegistry by lazy {PropertyChangeRegistry()}
-
-    override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
-        callbacks.remove(callback)
-    }
-
-    override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
-        callbacks.add(callback)
-    }
+class CurrentRouteViewModel() : ViewModel(){
 
 
-    val currentRouteInfo:LiveData<String>
-    get() = CurrentRouteRepo.currentRouteInfo
-
-
-   @Bindable
-   val editTextLocation = MutableLiveData<String>()
-
-
-
-    fun onSetARouteClick() = CurrentRouteRepo.getRouteInfo(editTextLocation.value)
 
 
 }
