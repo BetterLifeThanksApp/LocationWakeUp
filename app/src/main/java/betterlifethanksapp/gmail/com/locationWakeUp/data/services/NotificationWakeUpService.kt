64ab6715b2c0a0d624dc3f0a0d345ce46f9679ac
Maciej <betterlifethanksapp@gmail.com>
2020-03-05 14:08:00 +0100

    Add class with IntentService

---
+++ app/src/main/java/betterlifethanksapp/gmail/com/locationWakeUp/data/services/NotificationWakeUpService.kt
@@ -0,0 +1,12 @@
+package betterlifethanksapp.gmail.com.locationWakeUp.data.services
+
+import android.app.IntentService
+import android.content.Intent
+
+class NotificationWakeUpService: IntentService("NotificationWakeUpService") {
+
+
+    override fun onHandleIntent(intent: Intent?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
