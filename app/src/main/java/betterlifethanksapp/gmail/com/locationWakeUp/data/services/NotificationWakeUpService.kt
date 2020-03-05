95391cd9a6aa58c2e9d8a87bec04c1d6f7b01690
Maciej <betterlifethanksapp@gmail.com>
2020-03-05 15:33:18 +0100

    Create notify with vibraton and autoCancel

--- app/src/main/java/betterlifethanksapp/gmail/com/locationWakeUp/data/services/NotificationWakeUpService.kt
+++ app/src/main/java/betterlifethanksapp/gmail/com/locationWakeUp/data/services/NotificationWakeUpService.kt
@@ -1,19 +1,48 @@
 package betterlifethanksapp.gmail.com.locationWakeUp.data.services
 
+import android.R
 import android.app.IntentService
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.renderscript.RenderScript
 import android.util.Log
+import androidx.core.app.NotificationCompat
 
 class NotificationWakeUpService: IntentService("NotificationWakeUpService") {
 
     companion object{
         fun MESSAGE()  = "NotificationMessage"
+        fun NOTIFICATION_ID():Int = 9472
     }
 
 
     override fun onHandleIntent(intent: Intent?) {
         val text = intent?.getStringExtra(MESSAGE())
         Log.i("Intent","$text")
-        //createNotification()
+        if (text != null) {
+            createNotification(text)
+        }
+    }
+
+    private fun createNotification(text:String) {
+        val builder = createBuilder(text)
+        sendNotification(builder)
+
+    }
+
+    private fun sendNotification(builder: NotificationCompat.Builder) {
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.notify(NOTIFICATION_ID(),builder.build())
+    }
+
+    private fun createBuilder(text:String): NotificationCompat.Builder {
+        return NotificationCompat.Builder(applicationContext,"hello my ...")
+            .setSmallIcon(R.drawable.ic_dialog_map)
+            .setContentTitle("Tytuł")
+            .setContentText(text)
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+            .setVibrate(longArrayOf(0,2000,200,3000,500,1000,8000,600,400,300))
+            .setAutoCancel(true)
     }
 }
