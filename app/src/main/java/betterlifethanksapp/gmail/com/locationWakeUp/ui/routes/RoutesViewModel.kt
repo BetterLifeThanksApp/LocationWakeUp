92016e438fd7f48847a7846123c9395ad8ea8959
Maciej <betterlifethanksapp@gmail.com>
2020-03-20 19:43:28 +0100

    Get all location from Room using LiveData in RoutesViewModel

--- app/src/main/java/betterlifethanksapp/gmail/com/locationWakeUp/ui/routes/RoutesViewModel.kt
+++ app/src/main/java/betterlifethanksapp/gmail/com/locationWakeUp/ui/routes/RoutesViewModel.kt
@@ -2,6 +2,8 @@ package betterlifethanksapp.gmail.com.locationWakeUp.ui.routes
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import betterlifethanksapp.gmail.com.locationWakeUp.data.db.Location
 import betterlifethanksapp.gmail.com.locationWakeUp.data.db.LocationRoomDatabase
 
 class RoutesViewModel(application: Application)
@@ -9,12 +11,15 @@ class RoutesViewModel(application: Application)
 
     private val repository:RoutesRepository
 
+    val allLocation: LiveData<List<Location>>
+
 
     init{
         val locationDao = LocationRoomDatabase
             .getDatabase(application)
             .locationDao()
         repository = RoutesRepository(locationDao)
+        allLocation = repository.allLocation
     }
 
 
