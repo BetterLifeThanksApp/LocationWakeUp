25502fab5072172944da0854485214a4eccfff88
Maciej <betterlifethanksapp@gmail.com>
2020-03-24 13:05:18 +0100

    Change ViewModel to AndroidViewModel in settings

--- app/src/main/java/betterlifethanksapp/gmail/com/locationWakeUp/ui/settings/SettingsFragment.kt
+++ app/src/main/java/betterlifethanksapp/gmail/com/locationWakeUp/ui/settings/SettingsFragment.kt
@@ -21,13 +21,10 @@ class SettingsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        viewModel = ViewModelProviders.of(this).get(SettingsViewModel::class.java)
+
         return inflater.inflate(R.layout.settings_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(SettingsViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
 }
