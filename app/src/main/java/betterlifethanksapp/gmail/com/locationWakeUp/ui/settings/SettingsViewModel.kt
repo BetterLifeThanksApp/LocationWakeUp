321fe4c11b4b62ab73945f51bb2bc6958fe92301
Maciej <betterlifethanksapp@gmail.com>
2020-03-27 12:42:34 +0100

    Optimize imports in SettingsViewModel class

--- app/src/main/java/betterlifethanksapp/gmail/com/locationWakeUp/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/betterlifethanksapp/gmail/com/locationWakeUp/ui/settings/SettingsViewModel.kt
@@ -2,7 +2,6 @@ package betterlifethanksapp.gmail.com.locationWakeUp.ui.settings
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
 
 class SettingsViewModel(application: Application) : AndroidViewModel(application) {
     // TODO: Implement the ViewModel
